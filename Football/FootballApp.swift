cc3858546f1892dc83eea10d988d0755a6090a22
Carter Zielinski <zielinca@bc.edu>
2026-01-15 11:10:19 -0500

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Carter Zielinski on 1/15/26.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
